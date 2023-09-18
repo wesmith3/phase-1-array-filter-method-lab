dbb0b97e271c1519bab67f1c5d8b8ffe863ac17c
Wesley <wesmith314@gmail.com>
2023-09-18 07:38:35 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const lowerCaseName = name.toLowerCase()
+    const matchingDrivers = drivers.filter(driver => driver.toLowerCase() === lowerCaseName)
+    return matchingDrivers
+}
+
+function fuzzyMatch(drivers, letters) {
+    const matchingDrivers = drivers.filter(driver => {
+    const firstLetters = driver.slice(0, letters.length);
+    return firstLetters.toLowerCase() === letters.toLowerCase();
+  });
+  return matchingDrivers
+}
+
+function matchName(drivers, name) {
+    const matchingDrivers = drivers.filter(driver => {
+        if (driver.name === name)
+        return driver
+    } )
+    return matchingDrivers
+}
